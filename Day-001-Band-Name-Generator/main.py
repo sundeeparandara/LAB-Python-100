f867f442b5c327105a594f8916bdfd426793e14a
SLA <40652679+sundeeparandara@users.noreply.github.com>
2022-01-19 22:55:15 +0000

    day-1

---
+++ Day-001-Band-Name-Generator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? ")
+print(city)
+pet_name = input("What's your pet's name? ")
+print(pet_name)
+band_name = city + " " + pet_name
+print(f"Your band name could be {band_name}")
