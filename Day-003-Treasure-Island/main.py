1d5773749054865f25f6fabb38de0597a613fbf7
SLA <40652679+sundeeparandara@users.noreply.github.com>
2022-01-22 12:38:15 +0000

    update to storyline in main.py

--- Day-003-Treasure-Island/main.py
+++ Day-003-Treasure-Island/main.py
@@ -3,11 +3,11 @@ print(''.join([line for line in f]))
 
 
 print("Welcome to Treasure Island.\nYour mission is to find the treasure.\n")
-decision1 = input("left or right? ").lower()
+decision1 = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'. ").lower()
 if decision1 == "left":
-  decision2 = input("swim or wait? ").lower()
+  decision2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ").lower()
   if decision2 == "wait":
-    decision3 = input("which door (red, blue or yellow)? ")
+    decision3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
     if decision3 == "yellow":
       print("YOU WIN.")
     elif decision3 == "red":
